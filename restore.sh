04c014fa14471cbe8b7c1d6091ab677b508b9681
2nd_junkey <broken.phantsm@gmail.com>
2019-04-14 20:58:32 +0900

    add restore bat

---
+++ restore.sh
@@ -0,0 +1,7 @@
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install others
+brew cask install dropbox
+brew install mackup mas
+
